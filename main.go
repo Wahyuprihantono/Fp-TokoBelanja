004dd8745ec5a4fe92ed09e2767406c571309bc1
maulidtanaflah <maulidtakuliah@gmail.com>
2022-12-09 20:09:58 +0700

    transaction history

--- main.go
+++ main.go
@@ -19,14 +19,17 @@ func main() {
 	userRepository := repository.NewUserRepository(db)
 	categoryRepository := repository.NewCategoryRepository(db)
 	productRepository := repository.NewProductRepository(db)
+	transactionRepository := repository.NewTransactionRepository(db)
 
 	userService := service.NewUserService(userRepository)
 	categoryService := service.NewCategoryService(categoryRepository, productRepository)
 	productService := service.NewProductService(productRepository, categoryRepository)
+	transactionService := service.NewTransactionService(transactionRepository, userRepository, productRepository, categoryRepository)
 
 	userController := controller.NewUserController(userService)
 	categoryController := controller.NewCategoryController(categoryService)
 	productController := controller.NewProductController(productService)
+	transactionController := controller.NewTransactionController(transactionService)
 
 	// Users
 	router.POST("/users/register", userController.RegisterUser)
@@ -46,6 +49,11 @@ func main() {
 	router.GET("/products", middleware.AuthMiddleware, productController.GetAllProducts)
 	router.PUT("/products/:productId", middleware.AuthMiddleware, productController.UpdateProduct)
 	router.DELETE("/products/:productId", middleware.AuthMiddleware, productController.DeleteProduct)
+
+	// TransactionHistories
+	router.POST("/transactions", middleware.AuthMiddleware, transactionController.CreateTransaction)
+	router.GET("/transactions/my-transactions", middleware.AuthMiddleware, transactionController.GetUserTransactions)
+	router.GET("/transactions/user-transactions", middleware.AuthMiddleware, transactionController.GetAllTransactions)
 
 	port := os.Getenv("PORT")
 	if port == "" {
